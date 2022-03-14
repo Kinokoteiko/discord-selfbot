b19c70ab0f64d0bf6a541ebc856cfb4ed27bd627
RyuutenLoveCode <101356921+RyuutenLoveCode@users.noreply.github.com>
2022-03-14 18:48:27 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,7 @@
 from timeit import repeat
 import pyautogui, time
 time.sleep(this is the time you will define to open another tab.)
-f = open("zort", 'r')
+f = open("file name", 'r')
 for word in f:
     pyautogui.typewrite(word)
     time.sleep(time between each messages)
